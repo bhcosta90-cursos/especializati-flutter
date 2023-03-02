11a1da2734aa64cbff94d94b6acd7bd8002f6ead
Bruno Costa <bruno.costa@dafiti.com.br>
2023-03-02 14:25:13 -0300

    finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,34 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Text text = const Text("Texto");
+    TextField textField = const TextField();
+
     return MaterialApp(
         title: 'My App',
         home: Scaffold(
             appBar: AppBar(
-              title: Text("Widget App"),
+              title: const Text("Widget App"),
               centerTitle: true,
               backgroundColor: Colors.deepPurple,
             ),
-            body: const Padding(
-              padding: EdgeInsets.all(12),
-              child: Text('Widget App'),
+            body: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                children: [
+                  text,
+                  textField,
+                  Row(
+                    children: const [Text("Text 01"), Text("Text 02")],
+                  ),
+                  const Text(
+                    "Widget App",
+                    style: TextStyle(color: Colors.deepPurple),
+                  ),
+                  ElevatedButton(
+                      onPressed: () => {}, child: const Text("Widget App"))
+                ],
+              ),
             )));
   }
 }
