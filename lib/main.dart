ceb57a19679fb834579fbec71c35749eaeaf073d
Bruno Costa <bruno.costa@dafiti.com.br>
2023-03-02 13:45:26 -0300

    first commit with hello world in center

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'My App',
+        home: Scaffold(
+            appBar: AppBar(title: Text("My App")),
+            body: const Center(
+              child: Text('Olá'),
+            )));
+  }
+}
